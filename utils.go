72925d6c74c6bf0cbcb52e96c6dfe8ecb94a7502
Michalis Kargakis <michaliskargakis@gmail.com>
2014-08-04 21:57:27 +0300

    Create csv on the fly

--- utils.go
+++ utils.go
@@ -1,8 +1,13 @@
+// Copyright (c) 2014 Conformal Systems LLC.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
 	"encoding/csv"
 	"io"
+	"log"
 	"os"
 	"strconv"
 )
@@ -12,6 +17,43 @@ import (
 type Row struct {
 	k int
 	v int
+}
+
+// newCSV creates a new csv file containing a transaction curve
+// of <block #>, <txCount> fields
+func newCSV() error {
+	if _, err := os.Open(*txCurvePath); err == nil {
+		// We don't want to overwrite any existing csv file
+		return nil
+	}
+
+	// csv file does not exist so we can create a new one
+	file, err := os.Create(*txCurvePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	log.Println("Creating new csv file...")
+
+	write := csv.NewWriter(file)
+	var k, block int = (*maxBlocks - *matureBlock) / 4, 0
+	for i := *matureBlock + 1; i <= *maxBlocks; i++ {
+		record := []string{strconv.Itoa(i), strconv.Itoa(*tpb)}
+		if err := write.Write(record); err != nil {
+			return err
+		}
+		write.Flush()
+		block++
+
+		// Quadruple tpb every k blocks
+		if block%k == 0 {
+			*tpb *= 4
+		}
+	}
+
+	log.Println("New csv file created.")
+	return nil
 }
 
 // readCSV reads the given filename and
